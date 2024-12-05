c34ae72500fbe556439ecddb6f2f94d922d06d96
Hamed Ghazali <hamed.ghazali@digikala.com>
2024-12-05 13:34:14 +0330

    add docker file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // TODO: uncomment the below line if you want to build it with docker, since i use vercel for deployment, i didn't need to.
+  // output: "standalone",
+};
 
 export default nextConfig;
